320bedcfb1754d8bb73768e504c56a35c0f8e6f5
Gulaeva <137395490+Gulaeva@users.noreply.github.com>
2023-06-22 13:20:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Directory: ");
+        string directory = Console.ReadLine();
+        Console.Write("Regular expression: ");
+        string regex = Console.ReadLine();
+
+        if (!Directory.Exists(directory))
+        {
+            Console.WriteLine("Directory does not exist: " + directory);
+            return;
+        }
+
+        Console.WriteLine("\nMatches:");
+
+        try
+        {
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                if (Regex.IsMatch(Path.GetFileName(file), regex))
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error searching directory: " + ex.Message);
+        }
+    }
+}
